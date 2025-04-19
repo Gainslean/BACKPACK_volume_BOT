af36f979fc67c6e79086b346e913a620731016b5
CRYPTOGAINS <133777947+Gainslean@users.noreply.github.com>
2025-04-19 19:38:30 +0300

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,27 @@
+
+
+tickers = "BTC", "SOL", "ETH" # name traide pair / название торговых пар
+
+
+number_acc = 7 #  number of accounts/  количество акаунтов
+
+
+time_acc_from = 20 # MIN minimum delay between accounts(minutes)/ минимальная задержка между акаунтнами(минуты)
+
+time_acc_to = 30 # MIN maximum delay between accounts(minutes)/ максимальная задержка между акаунтнами(минуты)
+
+
+trade_from = 5 # minimum number of trades per ac in one cycle / минимальное количество трейдов на ак в одном цикле
+
+trade_to = 10 # maximum number of trades per ac in one cycle / максимальное количество трейдов на ак в одном цикле
+
+
+time_trade_sleep_from = 100 # SEC minimum delay before closing a trades(seconds) / минимальная задержка перед закрытием сделки(секунды)
+
+time_trade_sleep_to = 500 # SEC maximum delay before closing a trades(seconds) / максимальная задержка перед закрытием сделки(секунды)
+
+
+time_after_trade_sleep_from = 3 # MIN minimum delay between trades(minutes) / минимальная задержка между трейдами(минуты)
+
+
+time_after_trade_sleep_to = 15 # MIN maximum delay between trades(minutes) /  максимальная задержка между трейдами(минуты)
